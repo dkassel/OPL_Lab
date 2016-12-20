0e9f03043ec5242c9c93e377a0deae1d6adf9266
dkassel <dani3l.ka@gmail.com>
2016-12-20 17:38:39 +0100

    insertionsort with prefetch erste Variante. Achtung möglicher fehler bei prefetch verhalten, weil eine tmp variable verwendert wird? Überprüfung notwendig.

--- src/algorithms/Insertionsort.h
+++ src/algorithms/Insertionsort.h
@@ -12,8 +12,8 @@ using namespace std;
 template<typename T, size_t SIZE>
 void insertionSortOnArray(array<T, SIZE> &a) {
     for (size_t i = 1; i < SIZE; i++) {
-        for (size_t j = i - 1; a[j] > a[j + 1]; j--) {
-            swap(a[j + 1], a[j]);
+        for (size_t j = i; j > 0 && a[j - 1] > a[j]; j--) {
+            swap(a[j], a[j - 1]);
         }
     }
 }
@@ -21,12 +21,48 @@ void insertionSortOnArray(array<T, SIZE> &a) {
 
 template<typename T, size_t SIZE>
 void insertionSortOnArrayWithPrefetch(array<T, SIZE> &a) {
+    const size_t cacheRange = CACHE_LINE_SIZE / sizeof(T);
+    size_t cacheCnt;
+
     for (size_t i = 1; i < SIZE; i++) {
-        __builtin_prefetch(&a[0]);
-        for (size_t j = i - 1; a[j] > a[j + 1]; j--) {
-            swap(a[j + 1], a[j]);
+        cacheCnt = 0;
+        for (size_t j = i; j > 0 && a[j - 1] > a[j]; j--) {
+            if (cacheCnt <= 0) {
+                __builtin_prefetch(&a[j - cacheRange]);
+                cacheCnt = cacheRange;
+            }
+            cacheCnt--;
+            swap(a[j], a[j - 1]);
         }
     }
 }
+
+/*
+template<typename T, size_t SIZE>
+void insertionSortOnArrayWithPrefetchOld(array<T, SIZE> &a) {
+    const size_t cacheRange = CACHE_LINE_SIZE / sizeof(T);
+    for (size_t i = 1; i < SIZE; i += cacheRange) {   // Iteriere über array in cache schritten bis zum ende.
+
+        __builtin_prefetch(&a[i]);  // Prefetch an aktueller stelle
+
+        for (size_t j = i - 1; j < i + cacheRange && j < SIZE; j++) {
+            if (j + 1 < SIZE && a[j] > a[j + 1])
+                swap(a[j + 1], a[j]);
+            else break;
+        }
+
+
+        for (size_t k = i; k < i + cacheRange && k < SIZE; k++) {
+
+
+            for (size_t j = i - 1; a[j] > a[j + 1]; j--) {
+                swap(a[j + 1], a[j]);
+            }
+            cacheCounter++;
+        }
+
+    }
+}
+*/
 
 #endif //OPL_INSERTIONSORT_H
